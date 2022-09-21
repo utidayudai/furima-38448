beab7baedc52dda45faac5b292cb10ff1468dd84
utidayudai <s.luv.26.0720@gmail.com>
2022-09-21 22:04:00 +0900

    ユーザー登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #validstes :nickname,presence: true
+  #validstes :last_name,presence: true
+  #validstes :first_name,presence: true
+  #validstes :kana_last_name,presence: true
+  #validstes :kana_first_name,presence: true
+  #validstes :birthday,presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   get 'items/index'
-  resources :items, only: :index
+  resources :user, only: [:edit, :update]
 end
