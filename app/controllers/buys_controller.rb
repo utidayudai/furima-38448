187aced9898ff935cd63f6dbd4981c23aa49ad97
utidayudai <s.luv.26.0720@gmail.com>
2022-10-03 16:10:52 +0900

    バリデーション

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,2 +1,20 @@
 class BuysController < ApplicationController
+  def index
+  end
+
+  def create
+    @buy = Buy.create(buy_params)
+    Buy_record.create(buy_record_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def buy_params
+    params.permit(:place_id, :city, :address_number, :address, :building, :phone_number).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    
+  end
 end
