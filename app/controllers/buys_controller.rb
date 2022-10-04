b6eb8b8b6eb69b69ea2b988cd1535433d328e5d0
utidayudai <s.luv.26.0720@gmail.com>
2022-10-04 17:34:42 +0900

    データ保存

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -3,11 +3,11 @@ class BuysController < ApplicationController
   before_action :buy_set_item, only:[:index, :create]
 
   def index
-    @buy_buy_record = BuyBuy_record.new
+    @buy_buy_record = BuyBuyRecord.new
   end
 
   def create
-    @buy_buy_record = BuyBuy_record.new(buy_params)
+    @buy_buy_record = BuyBuyRecord.new(buy_params)
     if @buy_buy_record.valid?
       @buy_buy_record.save
       redirect_to root_path
@@ -19,10 +19,11 @@ class BuysController < ApplicationController
   private
 
   def buy_params
-    params.require(:buy_buy_record).permit(:place_id, :address_number, :address, :building, :phone_number).merge(user_id: current_user.id, sell_id: params[:sell_id])
+    params.require(:buy_buy_record).permit(:place_id, :city, :address_number, :address, :building, :phone_number).merge(user_id: current_user.id, sell_id: params[:sell_id])
   end
 
   def buy_set_item
     @sell = Sell.find(params[:sell_id])
   end
 end
+##.require(:buy_buy_record)
--- app/models/buy_buy_record.rb
+++ app/models/buy_buy_record.rb
@@ -1,9 +1,10 @@
-class BuyBuy_record
+class BuyBuyRecord
   include ActiveModel::Model
-  attr_accessor :place._id, :city, :address, :address_number, :building, :phone_number, :user_id, :sell_id
+  attr_accessor :place_id, :city, :address, :address_number, :building, :phone_number, :user_id, :sell_id
 
   validates :place_id, numericality: {other_than: 0, message: "can't be blank"}
-  validates :user_id, :sell_id
+  validate  :user_id
+  validate  :sell_id
   with_options presence: { message: "can't be blank" } do
     validates :city
     validates :address_number
@@ -12,7 +13,7 @@ class BuyBuy_record
   end
 
   def save
-    @buy = Buy_record.create(user_id: user.id, sell_id: params[:sell_id])
-    Buy.create(place_id: place_id, city: city, address_number: address_number, address: address, building: building, phone_number: phone_number)
+    buy_record = BuyRecord.create(user_id: user_id, sell_id: sell_id)  
+    Buy.create(place_id: place_id, city: city, address_number: address_number, address: address, building: building, phone_number: phone_number, buy_record_id: buy_record.id)
   end
 end
